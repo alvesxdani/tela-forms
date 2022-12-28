dc50ffe72da1baf6d9fd3bc4066242463d5d417b
alvesxdani <dani.alvesm22@gmail.com>
2022-12-28 16:13:25 -0300

    Add form

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -1,9 +1,40 @@
-import { FormsContent } from "./style";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { DivForm, FormsContent } from "./style";
+
+type FormValues = {
+  fullName: string;
+  userEmail: string;
+  userTel: number;
+  dateBirth: number;
+  userState: string;
+  bairroInteresse: string;
+  periodoContato: string;
+}
 
 const Forms = (props:any): JSX.Element => {
+  const {register, handleSubmit, formState: { errors }} = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = data => console.log(data);
   return(
     <FormsContent>
-      Teste
+      <h1>Não encontrou o que procurava?</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <DivForm>
+        <label>Nome: </label>
+        <input {...register("fullName")} placeholder="Seu nome completo"/>
+        </DivForm>
+
+        <DivForm>
+        <label>E-mail: </label>
+        <input {...register("userEmail")} placeholder="Seu melhor e-mail" />
+        </DivForm>
+
+        <DivForm>
+        <input type="submit" className="enviar"/>
+        </DivForm>
+      </form>
+
     </FormsContent>
   );
 }
--- src/components/Forms/style.ts
+++ src/components/Forms/style.ts
@@ -1,9 +1,46 @@
 import styled from "styled-components";
 
 export const FormsContent = styled.div`
-background: #3d3d3d;
+background: #2c2c2c;
 color: #fff;
 width: 500px;
 padding: 2rem;
 font: 0.8rem 'Open Sans', serif;
+border-radius: 1rem;
+  h1 {
+    font-family: 'Nunito', sans-serif;
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+`
+
+export const DivForm = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 0.8rem;
+  /* border: 1px solid #eee; */
+
+  label {
+    font-weight: bold;
+  }
+
+  input {
+    flex: 1;
+    margin-left: 0.8rem;
+    padding: 0.8rem;
+    background: #eee2;
+    color: #fff;
+    border-radius: 0.5rem;
+    border: none;
+  }
+
+  input.enviar {
+    flex: 0;
+    width: 50%;
+    margin-top: 1rem;
+    cursor: pointer;
+    padding: 1rem;
+  }
 `
